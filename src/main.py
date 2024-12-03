f9aa2da3a712b5ab1a3155063b38dfb711768600
Sergio Ramirez <sergio_ra94@hotmail.com>
2024-12-02 23:35:01 -0500

    add total_amount_transfer

--- src/main.py
+++ src/main.py
@@ -181,12 +181,12 @@ def process_message(message):
         output_key = f"results/{batch_id}_results.xlsx"
         save_excel_to_s3(df, output_key)
         has_failures = "FAILED" in df["status_transaction"].values
-        total_ammount_transfer = df[df["status_transaction"]=='SUCCESSFUL']['amount'].sum()
+        total_amount_transfer = df[df["status_transaction"]=='SUCCESSFUL']['amount'].sum()
         total_accounts_transfer = len(df[df["status_transaction"]=='SUCCESSFUL'])
         response_message = {
             "batch_id": batch_id,
             "status": "INCOMPLETE" if has_failures else "COMPLETE",
-            "total_ammount_transfer":total_ammount_transfer,
+            "total_amount_transfer":total_amount_transfer,
             "s3_file_path": output_key,
             "total_accounts_transfer":total_accounts_transfer
             
