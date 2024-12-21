8e27f353820f3fb02e402238a32cc5be8438a694
Sergio Ramirez <sergio_ra94@hotmail.com>
2024-12-20 23:05:31 -0500

    Replace print statement with error logging for user creation response

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ s3_client = boto3.client("s3")
 sqs_client = boto3.client("sqs")
 
 def async_request(data):
-    print(data)
+    logging.error(f"Response create user: {str(data)}")
     response = requests.post(URL_CREATE_USERS, data = data)
 
     logging.error(f"Response create user: {response.text}")
